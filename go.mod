fe077dea5269ca1c2ef553dbdaa59919ed48077c
jing.kang <kang.jing@varlook.com>
2021-09-07 18:04:45 -0700

    add ssh access remote db

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module webjwt-validate
 
 go 1.17
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jamf/go-mysqldump v0.6.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+)
--- service_conn.go
+++ service_conn.go
@@ -1,9 +1,11 @@
 package main
 
-// https://blog.logrocket.com/creating-a-web-server-with-golang/
+// JK 090721
+// SERVICE 1812 user.anfuying.com 3 root Dev00p123 22
 
 import (
 	"crypto/tls"
+	"database/sql"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -11,10 +13,111 @@ import (
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
 )
 
-var PORT = "1812"
 var GATE = "bad.lt.net"
+var PORT = "1812"
+var ZUHU = "1"
+var SSHU = "root"
+var SSHW = "Dev00p123"
+var SSHP = "22"
+
+const SQLU = "select id,username,full_name,email,phone,login_date,login_jwt from user where is_enabled = 1 and activated = 1 and is_deleted = 0 and role='user' and tenant_id = "
+
+type User struct {
+	id			int64
+	username	string
+	full_name	string
+	email		string
+	phone		string
+	login_date	string
+	login_jwt	string
+}
+
+type ViaSSHDialer struct {
+	client *ssh.Client
+}
+
+func (self *ViaSSHDialer) Dial(addr string) (net.Conn, error) {
+	return self.client.Dial("tcp", addr)
+}
+
+func sshConnection(w http.ResponseWriter, r *http.Request) {
+	dbUser := "root"           // DB username
+	dbPass := ""               // DB Password
+	dbHost := "localhost:3306" // DB Hostname/IP
+	dbName := "tistargate"     // Database name
+
+	var agentClient agent.Agent
+	// Establish a connection to the local ssh-agent
+	if conn, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
+		defer conn.Close()
+
+		// Create a new instance of the ssh agent
+		agentClient = agent.NewClient(conn)
+	}
+
+	// The client configuration with configuration option to use the ssh-agent
+	sshConfig := &ssh.ClientConfig{
+		User:            SSHU,
+		Auth:            []ssh.AuthMethod{},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	// When the agentClient connection succeeded, add them as AuthMethod
+	if agentClient != nil {
+		sshConfig.Auth = append(sshConfig.Auth, ssh.PublicKeysCallback(agentClient.Signers))
+	}
+
+	// When there's a non empty password add the password AuthMethod
+	if SSHW != "" {
+		sshConfig.Auth = append(sshConfig.Auth, ssh.PasswordCallback(func() (string, error) {
+			return SSHW, nil
+		}))
+	}
+
+	// Connect to the SSH Server
+	sshcon, err := ssh.Dial("tcp", fmt.Sprintf("%s:%s", GATE, SSHP), sshConfig)
+	if err != nil {
+		fmt.Fprintf(w, "ssh conn err: %v", err)
+		return
+	}
+	defer sshcon.Close()
+	fmt.Printf("connected to the host %s %s\n", GATE, SSHP)
+
+	// Now we register the ViaSSHDialer with the ssh connection as a parameter
+	mysql.RegisterDial("mysql+tcp", (&ViaSSHDialer{sshcon}).Dial)
+
+	// And now we can use our new driver with the regular mysql connection string tunneled through the SSH connection
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@mysql+tcp(%s)/%s", dbUser, dbPass, dbHost, dbName))
+	if err != nil {
+		fmt.Fprintf(w, "db ssh conn err: %v", err)
+		return
+	}
+	fmt.Printf("connected to the db %s@%s\n", dbHost, dbName)
+
+	rows, err := db.Query(SQLU + ZUHU)
+	if err != nil {
+		fmt.Fprintf(w, "data query err: %v", err)
+		return
+	}
+	//fmt.Printf(SQLU + ZUHU + "\n")
+
+	for rows.Next() {
+		var user User
+		err = rows.Scan(&user.id, &user.username, &user.full_name, &user.email, &user.phone, &user.login_date, &user.login_jwt)
+		if err != nil {
+			fmt.Printf("data row err: %v", err)
+			return
+		}
+		fmt.Fprintf(w, "%d,%s,%s,%s,%s,%s,%s\n", user.id, user.username, user.full_name, user.email, user.phone, user.login_date, user.login_jwt)
+	}
+	rows.Close()
+}
 
 func helpHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/help" {
@@ -47,7 +150,11 @@ func helpHandler(w http.ResponseWriter, r *http.Request) {
 
 	exa := "curl 'http://" + host +":1812/validate' -H 'Authorization: Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJkZXZvcDAxQHBlZ2FzdXMwMUB1c2VyIiwiYXV0aCI6IlJPTEVfVEVOQU5UIiwiZXhwIjoxNjMwOTEzOTc5fQ.7XXsWUis1zh1EmLN6XPIOiglp6o_7k0aU8FR1DYQvz6fFg-s5eprUAco6aEScwNavye3u9r3VRSrnI_okEaxpQ'"
 	fmt.Fprintf(w,"put jwt in reqest header to validate - Authorization \n\n")
-	fmt.Fprintf(w,"%s\n", exa)
+	fmt.Fprintf(w,"%s\n\n", exa)
+
+	fmt.Fprintf(w,"Service Start:\n")
+	fmt.Fprintf(w,"  %s GATEWAY_DOMAIN SERVICE_PORT TENANT_ID SSH_USER SSH_PASSWORD SSH_PORT\n", os.Args[0])
+	fmt.Fprintf(w,"  example:\n  %s %s %s %s %s %s %s \n", os.Args[0], GATE, PORT, ZUHU, "USR", "PWD", SSHP)
 }
 
 func sendJWTreq(jwt string) string {
@@ -55,7 +162,8 @@ func sendJWTreq(jwt string) string {
 
 	req, err := http.NewRequest("GET", req_url, nil)
 	if err != nil {
-		log.Fatalln(err)
+		log.Printf(err.Error())
+		return "{}"
 	}
 
 	if ! strings.Contains(jwt, "Bearer ") {
@@ -67,14 +175,16 @@ func sendJWTreq(jwt string) string {
 	client := &http.Client{Transport: tr}
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatalln(err)
+		log.Printf(err.Error())
+		return "{}"
 	}
 
 	fmt.Printf("%s\n%s\n%s\n", req_url, jwt, req.Header)
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln(err)
+		log.Printf(err.Error())
+		return "{}"
 	}
 	return string(body)
 }
@@ -111,13 +221,51 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s\n", resp_body)
 }
 
+func dataServices(w http.ResponseWriter, r *http.Request) {
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/tistargate?charset=utf8")
+	if err != nil {
+		fmt.Fprintf(w, "data source err: %v", err)
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query(SQLU + ZUHU)
+	if err != nil {
+		fmt.Fprintf(w, "data query err: %v", err)
+		return
+	}
+
+	for rows.Next() {
+		var user User
+		err = rows.Scan(&user.id, &user.username, &user.full_name, &user.email, &user.phone, &user.login_date, &user.login_jwt)
+		if err != nil {
+			fmt.Printf("data row err: %v", err)
+			return
+		}
+		fmt.Fprintf(w, "%d,%s,%s,%s,%s,%s,%s\n", user.id, user.username, user.full_name, user.email, user.phone, user.login_date, user.login_jwt)
+	}
+	rows.Close()
+}
+
 func main() {
-	if len(os.Args) == 2  {
+	if len(os.Args) > 1 {
 		GATE = os.Args[1]
 	}
-	if len(os.Args) == 3  {
-		GATE = os.Args[1]
+	if len(os.Args) > 2 {
 		PORT = os.Args[2]
+	}
+	if len(os.Args) > 3 {
+		ZUHU = os.Args[3]
+	}
+	if len(os.Args) > 4 {
+		SSHU = os.Args[4]
+	}
+	if len(os.Args) > 5 {
+		SSHW = os.Args[5]
+	}
+	if len(os.Args) > 6 {
+		SSHP = os.Args[6]
 	}
 
 	fileServer := http.FileServer(http.Dir("./static"))
@@ -126,6 +274,8 @@ func main() {
 	http.HandleFunc("/test", formHandler)
 	http.HandleFunc("/help", helpHandler)
 	http.HandleFunc("/validate", jwtValidateHandler)
+	http.HandleFunc("/services", dataServices)
+	http.HandleFunc("/remotecs", sshConnection)
 
 	fmt.Printf("Server Port %s, Gateway %s\n", PORT, GATE)
 
@@ -133,3 +283,7 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
+// https://tutorialedge.net/golang/golang-mysql-tutorial/
+// https://blog.logrocket.com/creating-a-web-server-with-golang/
+// https://gist.github.com/vinzenz/d8e6834d9e25bbd422c14326f357cce0
