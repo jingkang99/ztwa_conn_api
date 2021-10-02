a32588d86d664d4d16de880b38e8a19fef2173f1
jing.kang <kang.jing@varlook.com>
2021-10-02 16:59:48 -0700

    add file upload test

--- go.mod
+++ go.mod
@@ -3,8 +3,16 @@ module webjwt-validate
 go 1.17
 
 require (
+	github.com/carlescere/scheduler v0.0.0-20170109141437-ee74d2f83d82 // indirect
+	github.com/go-co-op/gocron v1.9.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang/glog v1.0.0 // indirect
 	github.com/jamf/go-mysqldump v0.6.0 // indirect
+	github.com/jasonlvhit/gocron v0.0.1 // indirect
+	github.com/prprprus/scheduler v0.5.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/schollz/websocket v1.8.0 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 )
--- service_conn.go
+++ service_conn.go
@@ -5,15 +5,20 @@ package main
 
 import (
 	"context"
+	"crypto/hmac"
+	"crypto/sha1"
 	"crypto/tls"
 	"database/sql"
+	"encoding/base32"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"strings"
+	"text/template"
 	"time"
 
 	"github.com/go-sql-driver/mysql"
@@ -30,6 +35,9 @@ var ZUHU = "1"
 var SSHU = "root"
 var SSHW = "Dev00p123"
 var SSHP = "22"
+
+// Compile templates on start of the application
+var templates = template.Must(template.ParseFiles("static/upload.html"))
 
 const SQLU = "select id,username,full_name,email,phone,login_date,login_jwt from user where is_enabled = 1 and activated = 1 and is_deleted = 0 and role='user' and tenant_id = "
 
@@ -298,6 +306,14 @@ func perfTestBench(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "ok")
 }
 
+func oneTimePwdGen(w http.ResponseWriter, r *http.Request) {
+	pwd, secondsRemaining := otpGen(GATE)
+	html := "<head><meta http-equiv=\"refresh\" content=\"10\" /></head>"
+
+	fmt.Fprintf(w,"%s\n", html)
+	fmt.Fprintf(w,"<body>%06d &#8592; %d second(s) remaining</body>\n", pwd, secondsRemaining)
+}
+
 func sche_task(t time.Time ) {
 	fmt.Println("task ", t)
 }
@@ -311,6 +327,119 @@ func scheduleT(ticker *time.Ticker, done chan bool  ) {
 			sche_task(t)
 		}
 	}
+}
+
+func toBytes(value int64) []byte {
+	var result []byte
+	mask := int64(0xFF)
+	shifts := [8]uint16{56, 48, 40, 32, 24, 16, 8, 0}
+	for _, shift := range shifts {
+		result = append(result, byte((value>>shift)&mask))
+	}
+	return result
+}
+
+func toUint32(bytes []byte) uint32 {
+	return (uint32(bytes[0]) << 24) + (uint32(bytes[1]) << 16) +
+		(uint32(bytes[2]) << 8) + uint32(bytes[3])
+}
+
+func oneTimePassword(key []byte, value []byte) uint32 {
+	// sign the value using HMAC-SHA1
+	hmacSha1 := hmac.New(sha1.New, key)
+	hmacSha1.Write(value)
+	hash := hmacSha1.Sum(nil)
+
+	// We're going to use a subset of the generated hash.
+	// Using the last nibble (half-byte) to choose the index to start from.
+	// This number is always appropriate as it's maximum decimal 15, the hash will
+	// have the maximum index 19 (20 bytes of SHA1) and we need 4 bytes.
+	offset := hash[len(hash)-1] & 0x0F
+
+	// get a 32-bit (4-byte) chunk from the hash starting at offset
+	hashParts := hash[offset : offset+4]
+
+	// ignore the most significant bit as per RFC 4226
+	hashParts[0] = hashParts[0] & 0x7F
+
+	number := toUint32(hashParts)
+
+	// size to 6 digits
+	// one million is the first number with 7 digits so the remainder
+	// of the division will always return < 7 digits
+	pwd := number % 1000000
+
+	return pwd
+}
+
+// Display the named template
+func display(w http.ResponseWriter, page string, data interface{}) {
+	templates.ExecuteTemplate(w, page+".html", data)
+}
+
+func uploadFileHdl(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		display(w, "upload", nil)
+	case "POST":
+		uploadFile(w, r)
+	}
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	// Maximum upload of 10 MB files
+	r.ParseMultipartForm(10 << 20)
+
+	// Get handler for filename, size and headers
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	// Create file
+	dst, err := os.Create("temp/" + handler.Filename)
+	defer dst.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Copy the uploaded file to the created file on the filesystem
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
+}
+
+// all []byte in this program are treated as Big Endian
+// https://github.com/robbiev/two-factor-auth
+func otpGen(input string) (uint32, int64) {
+	input = strings.Replace(os.Args[1], " ", "", -1)
+	input = strings.Replace(input, ".", "", -1)
+	input = strings.ToUpper(input)
+
+	key, err := base32.StdEncoding.WithPadding(base32.NoPadding).DecodeString(input)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	// generate a one-time password using the time at 30-second intervals
+	epochSeconds := time.Now().Unix()
+	pwd := oneTimePassword(key, toBytes(epochSeconds/30))
+
+	secondsRemaining := 30 - (epochSeconds % 30)
+	fmt.Printf("%06d (%d second(s) remaining)\n", pwd, secondsRemaining)
+	return pwd, secondsRemaining
 }
 
 func main() {
@@ -333,6 +462,26 @@ func main() {
 		SSHP = os.Args[6]
 	}
 
+	for i:= 3; i>=0; i-- {
+		fmt.Printf("\033[2K\r%d", i)
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Println()
+
+	/* // https://gobyexample.com/tickers
+	ticker := time.NewTicker(5000 * time.Millisecond)
+	done := make(chan bool)
+
+	for {
+		select {
+		case <-done:
+			return
+		case t := <-ticker.C:
+			sche_task(t)
+		}
+	}
+	//scheduleT(ticker, done)  */
+
 	fileServer := http.FileServer(http.Dir("./static"))
 
 	mux := http.NewServeMux()
@@ -344,6 +493,8 @@ func main() {
 	mux.HandleFunc("/services", dataServices)
 	mux.HandleFunc("/remotecs", sshConnection)
 	mux.HandleFunc("/webscket", connWebsocket)
+	mux.HandleFunc("/otpasswd", oneTimePwdGen)
+	mux.HandleFunc("/myupload", uploadFileHdl)
 
 	fmt.Printf("Server Port %s, Gateway %s\n", PORT, GATE)
 
@@ -360,3 +511,5 @@ func main() {
 
 // "github.com/carlescere/scheduler"
 // "github.com/go-co-op/gocron"
+
+// https://gabrieltanner.org/blog/golang-file-uploading
